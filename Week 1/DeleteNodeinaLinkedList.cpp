51de6076192c050430e69afdf4b19e37abe9805b
Priyanka T <tpriyanka1998@gmail.com>
2020-06-04 20:20:03 +0530

    Create DeleteNodeinaLinkedList.cpp

---
+++ Week 1/DeleteNodeinaLinkedList.cpp
@@ -0,0 +1,25 @@
+/*
+Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.
+
+Example 1:
+Input: head = [4,5,1,9], node = 5
+Output: [4,1,9]
+Explanation: You are given the second node with value 5, the linked list should become 4 -> 1 -> 9 after calling your function.
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+      ListNode* temp=node->next;
+      node->val=temp->val;
+      node->next=temp->next;
+      delete temp;
+    }
+};
