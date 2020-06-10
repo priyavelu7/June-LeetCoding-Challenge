96ab0a69658f20edd09c84e3628cdb918744c337
Priyanka T <tpriyanka1998@gmail.com>
2020-06-10 17:40:52 +0530

    Create SearchInsertPosition.cpp

---
+++ Week 2/SearchInsertPosition.cpp
@@ -0,0 +1,23 @@
+/*
+Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+You may assume no duplicates in the array.
+
+Example 1:
+Input: [1,3,5,6], 5
+Output: 2
+*/
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+      int left=0,right=nums.size()-1;
+      while(left<=right){
+        int mid=left+(right-left)/2;
+        if(nums[mid]==target)
+          return mid;
+        else if(nums[mid]>target)
+          right=mid-1;
+        else left=mid+1;
+      }
+      return left;
+    }
+};
